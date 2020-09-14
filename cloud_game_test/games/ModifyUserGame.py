381edbf1d3cde664fb6f2fe8a0ab56a0fd562fd0
ppeterzhao <ppeterzhao@tencent.com>
2020-09-15 00:20:10 +0800

    更新用例

--- cloud_game_test/games/ModifyUserGame.py
+++ cloud_game_test/games/ModifyUserGame.py
@@ -87,6 +87,145 @@ class ModifyUserGameTestCasegameidnull(ClougGameTestCaseBase):
         self.assert_http_ok('HTTP状态码必须为200', resp)
         #=====================
 
+#### class ModifyUserGameTestCasedesoverlong(ClougGameTestCaseBase):
+    '''ModifyUserGame 描述超长
+    '''
+    owner = "ppeterzhao"
+    timeout = 5
+    priority = TestCase.EnumPriority.High
+    status = TestCase.EnumStatus.Ready
 
+    def pre_test(self):
+        self.start_step('通过DescribeUserGameList选定一个期望游戏')
+        games = self.api3_gs_helper.list_user_games()
+        self.game = games[0] if games else None
+        self.game_id = get_by_path(self.game, 'GameId', None)
+
+    def run_test(self):
+        game_id = getattr(self, 'game_id', None)
+        if not game_id:
+            self.fail('选定期望游戏失败, 游戏列表可能为空, 或者接口返回错误')
+            return
+
+        # ==========
+        self.start_step('ModifyUserGame, GameId={}'.format(game_id))
+        self.game['Description'] = 'AUTOTEST-{}'.format(str(uuid.uuid4()))
+        params = {
+            'GameId': None,
+            'Description': 
+            self.game['Description']
+        }
+        resp = self.api3client.call('ModifyUserGame', params)
+
+        # ==========
+        self.start_step('检查返回')
+        self.assert_http_ok('HTTP状态码必须为200', resp)
+        #=====================
+
+###class ModifyUserGameTestCasestateoffline(ClougGameTestCaseBase):
+    '''ModifyUserGame stateoffline
+    '''
+    owner = "ppeterzhao"
+    timeout = 5
+    priority = TestCase.EnumPriority.High
+    status = TestCase.EnumStatus.Ready
+
+    def pre_test(self):
+        self.start_step('通过DescribeUserGameList选定一个期望游戏')
+        games = self.api3_gs_helper.list_user_games()
+        self.game = games[0] if games else None
+        self.game_id = get_by_path(self.game, 'GameId', None)
+
+    def run_test(self):
+        game_id = getattr(self, 'game_id', None)
+        if not game_id:
+            self.fail('选定期望游戏失败, 游戏列表可能为空, 或者接口返回错误')
+            return
+
+        # ==========
+        self.start_step('ModifyUserGame, GameId={}'.format(game_id))
+        self.game['Description'] = 'AUTOTEST-{}'.format(str(uuid.uuid4()))
+        params = {
+            'GameId': None,
+            'Description': 
+            self.game['Description']
+        }
+        resp = self.api3client.call('ModifyUserGame', params)
+
+        # ==========
+        self.start_step('检查返回')
+        self.assert_http_ok('HTTP状态码必须为200', resp)
+        #=====================
+
+###class ModifyUserGameTestCasegameinstancetype(ClougGameTestCaseBase):
+    '''ModifyUserGame instancetype正常
+    '''
+    owner = "ppeterzhao"
+    timeout = 5
+    priority = TestCase.EnumPriority.High
+    status = TestCase.EnumStatus.Ready
+
+    def pre_test(self):
+        self.start_step('通过DescribeUserGameList选定一个期望游戏')
+        games = self.api3_gs_helper.list_user_games()
+        self.game = games[0] if games else None
+        self.game_id = get_by_path(self.game, 'GameId', None)
+
+    def run_test(self):
+        game_id = getattr(self, 'game_id', None)
+        if not game_id:
+            self.fail('选定期望游戏失败, 游戏列表可能为空, 或者接口返回错误')
+            return
+
+        # ==========
+        self.start_step('ModifyUserGame, GameId={}'.format(game_id))
+        self.game['Description'] = 'AUTOTEST-{}'.format(str(uuid.uuid4()))
+        params = {
+            'GameId': None,
+            'Description': 
+            self.game['Description']
+        }
+        resp = self.api3client.call('ModifyUserGame', params)
+
+        # ==========
+        self.start_step('检查返回')
+        self.assert_http_ok('HTTP状态码必须为200', resp)
+        #=====================
+
+###class ModifyUserGameTestCasegameinstancetypeun(ClougGameTestCaseBase):
+    '''ModifyUserGame instancetype异常
+    '''
+    owner = "ppeterzhao"
+    timeout = 5
+    priority = TestCase.EnumPriority.High
+    status = TestCase.EnumStatus.Ready
+
+    def pre_test(self):
+        self.start_step('通过DescribeUserGameList选定一个期望游戏')
+        games = self.api3_gs_helper.list_user_games()
+        self.game = games[0] if games else None
+        self.game_id = get_by_path(self.game, 'GameId', None)
+
+    def run_test(self):
+        game_id = getattr(self, 'game_id', None)
+        if not game_id:
+            self.fail('选定期望游戏失败, 游戏列表可能为空, 或者接口返回错误')
+            return
+
+        # ==========
+        self.start_step('ModifyUserGame, GameId={}'.format(game_id))
+        self.game['Description'] = 'AUTOTEST-{}'.format(str(uuid.uuid4()))
+        params = {
+            'GameId': None,
+            'Description': 
+            self.game['Description']
+        }
+        resp = self.api3client.call('ModifyUserGame', params)
+
+        # ==========
+        self.start_step('检查返回')
+        self.assert_http_ok('HTTP状态码必须为200', resp)
+        #=====================
+        
 if __name__ == '__main__':
     ModifyUserGameTestCase().debug_run()
